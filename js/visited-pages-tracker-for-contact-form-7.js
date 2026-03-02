31cd8a1cb02d791d0a86f13ffa72c6187313c058
hidyk <hideyuki8546@gmail.com>
2026-03-02 23:10:17 +0900

    Use wpcf7beforesubmit event for reliable form data submission

--- js/visited-pages-tracker-for-contact-form-7.js
+++ js/visited-pages-tracker-for-contact-form-7.js
@@ -1,38 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // console.log("DOMContentLoaded event fired");
-
     var pageUrl = window.location.href;
     var visitedPages = JSON.parse(sessionStorage.getItem('visited_pages')) || [];
 
-    // console.log("Current page URL:", pageUrl);
-    // console.log("Visited pages before update:", visitedPages);
-
-    // adding the current page URL to visited_pages
     if (!visitedPages.includes(pageUrl)) {
         visitedPages.push(pageUrl);
     }
 
-    // console.log("Visited pages after update:", visitedPages);
-
-    // saving visited_pages to sessionStorage
     sessionStorage.setItem('visited_pages', JSON.stringify(visitedPages));
-    // console.log("Visited pages saved to sessionStorage");
+});
 
-    // adding visited_pages to the form data before submit event is fired
-    document.querySelectorAll('form.wpcf7-form').forEach(function(form) {
-        // console.log("Found form:", form);
-        form.addEventListener('submit', function() {
-            // console.log("Form submit event fired");
-            var visitedPages = sessionStorage.getItem('visited_pages');
-            // console.log("Visited pages from sessionStorage:", visitedPages);
-
-            var input = document.createElement('input');
-            input.type = 'hidden';
-            input.name = 'visited_pages';
-            input.value = visitedPages;
-            form.appendChild(input);
-
-            // console.log("Hidden input added:", input);
-        });
-    });
+document.addEventListener('wpcf7beforesubmit', function(event) {
+    event.detail.formData.append('visited_pages', sessionStorage.getItem('visited_pages'));
 });
